55f2e08f64bd36a3a810603506c1b6e82ee7a9dc
Oleh Barbashov <teosofio@gmail.com>
2025-06-03 17:15:11 +0300

    Added properties to the component employees-list-item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,11 @@ import './app.css';
 
 
 function App() {
+    const data = [
+        {name: "John C.", salary: 800, increase: false},
+        {name: "Oleh B.", salary: 3000, increase: true},
+        {name: "Anna Sh.", salary: 5000, increase: false},
+    ];
     return (
         <div className="app">
             <AppInfo />
@@ -16,7 +21,7 @@ function App() {
                 <AppFilter />
             </div>
 
-            <EmployeesList />
+            <EmployeesList data={data} />
 
             <EmployeesAddForm />
         </div>
